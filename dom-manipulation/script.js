c14cdc9a0ac7deb19e27acf87e303dc210065d59
yjoel1 <joelyeddu@gmail.com>
2025-12-14 20:48:43 +0000

    Delete dom-manipulation/script.js

--- dom-manipulation/script.js
+++
@@ -1,5 +0,0 @@
-  <div>
-    <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
-    <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
-    <button onclick="addQuote()">Add Quote</button>
-  </div>
