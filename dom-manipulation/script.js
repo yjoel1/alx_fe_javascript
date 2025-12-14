2db306cd3e5211effc619e96bf6530d763ab6fe4
yjoel1 <joelyeddu@gmail.com>
2025-12-14 20:54:09 +0000

    Add HTML structure for quote management

---
+++ dom-manipulation/script.js
@@ -0,0 +1,8 @@
+  <div>
+    <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
+    <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
+    <button onclick="addQuote()">Add Quote</button>
+    <div id="quoteDisplay"></div>
+    <button onclick="showRandomQuote()">Show Random Quote</button>
+    <script src="script.js"></script>
+    </div>
