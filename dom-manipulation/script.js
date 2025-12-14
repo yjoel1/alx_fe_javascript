3eb8844fccaccc86d82f37853340d14b3b0c17aa
yjoel1 <joelyeddu@gmail.com>
2025-12-14 20:39:53 +0000

    Add inputs for new quote and category

---
+++ dom-manipulation/script.js
@@ -0,0 +1,5 @@
+  <div>
+    <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
+    <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
+    <button onclick="addQuote()">Add Quote</button>
+  </div>
